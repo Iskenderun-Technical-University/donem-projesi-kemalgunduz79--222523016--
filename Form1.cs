b606a6b864f99e07ce74b923a0b441d3c135b8bb
kemal gunduz(222523016) <115525068+kemalgunduz79@users.noreply.github.com>
2023-05-22 21:47:30 +0300

    güncelleme Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,25 +1,88 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+using System;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
-namespace Geridon
+namespace GeriDon  //proje ismini geridön koymuştum.
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        private SqlConnection connection;
+        private string connectionString = "Your_Connection_String"; // Veritabanı bağlantı dizesini buraya girin
+
+        public MainForm()
         {
             InitializeComponent();
+
+            connection = new SqlConnection(connectionString);
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void MainForm_Load(object sender, EventArgs e)
         {
+            VerileriGuncelle();
+        }
 
+        private void btnMusteriEkle_Click(object sender, EventArgs e)
+        {
+            string ad = txtAd.Text;
+            string soyad = txtSoyad.Text;
+            string adres = txtAdres.Text;
+            int kazanilanPuan = Convert.ToInt32(txtKazanilanPuan.Text);
+            decimal odenenPara = Convert.ToDecimal(txtOdenenPara.Text);
+
+            try
+            {
+                connection.Open();
+
+                string insertQuery = "INSERT INTO Musteriler (Ad, Soyad, Adres, KazanilanPuan, OdenenPara) VALUES (@Ad, @Soyad, @Adres, @KazanilanPuan, @OdenenPara)";
+
+                using (SqlCommand command = new SqlCommand(insertQuery, connection))
+                {
+                    command.Parameters.AddWithValue("@Ad", ad);
+                    command.Parameters.AddWithValue("@Soyad", soyad);
+                    command.Parameters.AddWithValue("@Adres", adres);
+                    command.Parameters.AddWithValue("@KazanilanPuan", kazanilanPuan);
+                    command.Parameters.AddWithValue("@OdenenPara", odenenPara);
+
+                    command.ExecuteNonQuery();
+                }
+
+                MessageBox.Show("Müşteri başarıyla eklendi.");
+
+                VerileriGuncelle();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata oluştu: " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        private void VerileriGuncelle()
+        {
+            try
+            {
+                connection.Open();
+
+                string selectQuery = "SELECT * FROM Musteriler";
+
+                SqlDataAdapter adapter = new SqlDataAdapter(selectQuery, connection);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+
+                dgvMusteriler.DataSource = dataTable;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata oluştu: " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
         }
     }
 }
